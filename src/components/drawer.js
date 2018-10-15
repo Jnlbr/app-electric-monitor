f6e27d4b94406ff82339ba71909aa92cd2e5f5c4
Jean Lambert <jeeaan1702@gmail.com>
2018-10-15 14:33:17 -0400

    drawer component basis

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -9,28 +9,23 @@ import { DrawerItems, SafeAreaView } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from './Styles/drawer';
 
-class Drawer extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          <SafeAreaView
-            style={{flex:1}}
-            forceInset={{ top: 'always', horizontal: 'never' }}
-          ><DrawerItems activeTintColor="white" inactiveTintColor="white" {...this.props} />
-          </SafeAreaView>               
-        </ScrollView>
-        <View style={styles.footerContainer}>
-          {/* <TouchableOpacity
-            style={styles.button}
-            onPress={this.handleLogout}
-          ><Icon name="logout" size={30} color="white" />
-          </TouchableOpacity> */}
-        </View>
-      </View>
-    );
-  }
-}
+const Drawer = () => (
+  <View style={styles.container}>
+    <ScrollView>
+      <SafeAreaView
+        style={{flex:1}}
+        forceInset={{ top: 'always', horizontal: 'never' }}
+      ><DrawerItems activeTintColor="white" inactiveTintColor="white" {...this.props} />
+      </SafeAreaView>               
+    </ScrollView>
+    <View style={styles.footerContainer}>
+      {/* <TouchableOpacity
+        style={styles.button}
+        onPress={this.handleLogout}
+      ><Icon name="logout" size={30} color="white" />
+      </TouchableOpacity> */}
+    </View>
+  </View>
+);
 
 export default Drawer;
